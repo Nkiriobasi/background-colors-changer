131dabab14a901084e6ede061cd2055f12568564
Nkiriobasi <88588310+Nkiriobasi@users.noreply.github.com>
2022-03-10 11:44:34 +0100

    Updated main.js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 let index = 0;
 
+document.querySelector("button").addEventListener("click", changeColors);
+
+
+
 function changeColors(){
-    let colors = ["blue","green","orange","brown","purple","black","yellow","pink","maroon"];
+    let colors = ["blue","green","orange","brown","purple","black","yellow","pink","maroon","grey","Acid green","Alloy orange"];
 
     document.getElementsByTagName("body")[0].style.background = colors[index++];
 
